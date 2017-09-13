a041a7862800b1fab98a49a2f03aa3df542dfa60
Wayne-Briggs <wayne.briggs@savvytechnologies.co.uk>
2017-09-13 07:58:47 +0100

    Added more brightness options

---
+++ bright_plus17_Callback.m
@@ -0,0 +1,12 @@
+function bright_plus17_Callback(hObject, eventdata, handles)
+global brightness;
+brightness = 8.8;
+
+% Loads the previous frame to the currently displayed frame into the display
+pathname = evalin('base','seqhandles.seqdir;');
+filename = evalin('base','seqhandles.filename;');
+%image_no=evalin('base','seqhandles.image_no;');
+image_no = str2num(get(handles.image_no,'string'));
+
+displayImage(pathname,filename,image_no)
+
